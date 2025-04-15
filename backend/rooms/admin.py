46e771975f30d3ca12fda82704b550a102aaa757
Javier Guerrero <jeguerrerocordoba@gmail.com>
2025-04-15 12:43:28 -0500

    delete room from api url

--- backend/rooms/admin.py
+++ backend/rooms/admin.py
@@ -3,7 +3,7 @@ from .models import Room, Message
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ('code', 'host', 'is_active', 'created_at')
+    list_display = ('code', 'is_active', 'created_at')
     list_filter = ('is_active', 'created_at')
     search_fields = ('code', 'host__username')
     readonly_fields = ('code',)
