770cdfe3c892a80428a6dab64a9845ae7d9e74bd
Javier Guerrero <jeguerrerocordoba@gmail.com>
2025-04-23 13:33:49 -0500

    delete messages field when retrieving a room

--- backend/rooms/serializers.py
+++ backend/rooms/serializers.py
@@ -28,10 +28,10 @@ class MessageSerializer(serializers.ModelSerializer):
         fields = ['id', 'room', 'sender', 'content', 'created_at']
 
 class RoomSerializer(serializers.ModelSerializer):
-    messages = MessageSerializer(many=True, read_only=True)
+    #messages = MessageSerializer(many=True, read_only=True)
     class Meta:
         model = Room
-        fields = ['id', 'code', 'created_at', 'updated_at', 'drawing_data', 'shared_text', 'messages']
+        fields = ['id', 'code', 'created_at', 'updated_at', 'drawing_data', 'shared_text']
 
 class WebSocketTicketSerializer(serializers.ModelSerializer):
     class Meta:
